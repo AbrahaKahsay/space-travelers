375232fc1827c6b499dcff1db46e57d51c1bcd05
karam084 <karam.ezz84@gmail.com>
2022-08-23 21:21:39 +0200

    Add header and navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+    <nav className="navbar">
+        <h1>logo</h1>
+        <div className="links">
+            <Link to="/">Rockets</Link>
+            <Link to="/mission">Mission</Link>
+            <Link to="/profile">My Profile</Link>
+        </div>
+    </nav>
+);
+
+export default Navbar;
