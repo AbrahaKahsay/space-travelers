67f073c467e55f9768306b7a19dc2e456ed56457
Abraha Kahsay <abrahak81@gmail.com>
2022-08-25 02:00:39 +0300

    Fix errors and finish tasks

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,3 +1,4 @@
+import './Rockets.css';
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import fetchRocketsFromServer from '../../Redux/Rockets/Api';
@@ -13,9 +14,9 @@ const Rockets = ()=>{
   return(
   <div>
     {rockets.map((rocket) => (
-    <div key={rocket.id}>
-      <img src={rocket.images[0]} alt="Rocket image" />
-      <div>
+    <div className='rocket-container' key={rocket.id}>
+      <div className='rocket-image'><img src={rocket.images[0]} alt="Rocket image" /></div>
+      <div className='name-descr-container'>
         <h2 id={rocket.id}>{rocket.name}</h2>
         <p> {rocket.description}</p>
       </div>
