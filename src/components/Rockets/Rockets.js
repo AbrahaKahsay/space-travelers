7222261a51d55541f72bd7f3b8d47351b17f0eed
Abraha Kahsay <abrahak81@gmail.com>
2022-08-24 23:52:44 +0300

    Add elements to Rockets component

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import fetchRocketsFromServer from '../../Redux/Rockets/Api';
 
 const Rockets = ()=>{
+    const dispatch = useDispatch();
+    const rockets = useSelector((state) => state.Rockets);
+    useEffect(() => {
+        if (!rockets.length) {
+      dispatch(fetchRocketsFromServer());
+    }
+  }, []);
     return(
-        <div><p>This Rockets section</p></div>
-    )
+    <div>
+      {rockets.map((rocket) => (
+        <div key={rocket.id}>
+          <img src={rocket.images[0]} alt="Rocket" />
+          <div>
+            <h2 id={rocket.id}>{rocket.name}</h2>
+            <p> {rocket.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+    );
 }
 
 export default Rockets;
