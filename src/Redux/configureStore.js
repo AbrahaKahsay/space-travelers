eb1b8a82db6a46c9f34440271b41492887c62df2
Abraha Kahsay <abrahak81@gmail.com>
2022-08-24 23:12:56 +0300

    Add configureStore.js for rockets

---
+++ src/Redux/configureStore.js
@@ -0,0 +1,11 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import rocketsReducer from './Rockets/Rockets';
+const rootReducer = combineReducers({
+    Rockets: rocketsReducer,
+  });
+  
+  const store = configureStore({
+    reducer: rootReducer,
+  });
+  
+  export default store;
