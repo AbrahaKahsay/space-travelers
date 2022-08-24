b008579b410090f1487f124181ccb629fa922827
Abraha Kahsay <abrahak81@gmail.com>
2022-08-24 18:26:05 +0300

    Add style and Redux directory

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,10 @@ import Logo from '../components/planet.png';
 
 const Navbar = () => (
     <nav className="navbar">
-        <div className='logo'><img className='logo-img' src={Logo} alt="Logo" /></div>
+        <div className='logo-container'>
+            <div><img className='logo' src={Logo} alt="Logo" /></div>
+            <h1>{ "Space Travelers' Hub"} </h1>
+        </div>
         <div className="links">
            <ul>
             <li><Link to="/">Rockets</Link></li>
