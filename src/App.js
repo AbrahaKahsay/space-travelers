e57a6c9b035cf382ab186250f2679c11dc21b715
Abraha Kahsay <abrahak81@gmail.com>
2022-08-26 19:14:30 +0300

    Remove profile

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Missions from './components/Mission/Mission';
-import Profile from './components/profile/Profile';
 import Rockets from './components/Rockets/Rockets';
 
 function App() {
